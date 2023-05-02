30c0ca1f142c48ad2c89916772f93d2f50b966bd
Linxson <linxson0510@gmail.com>
2023-05-02 16:23:06 +0800

    change U-update

--- src/FVM/FVM_Water_hammer.m
+++ src/FVM/FVM_Water_hammer.m
@@ -8,7 +8,7 @@ global c;
 
 L=300;                          % 管线长度
 Hr=70;                          % 泵压力
-N=10;                           % 分段数
+N=25;                           % 分段数
 NS=N+1;                         % 节点数
 e=0.001651;                     % 壁厚m,0.065''
 D=0.00635-2*e;	                % 管道内径
@@ -26,17 +26,19 @@ t_max=20;                       % 总计算时间
 c=sqrt(K/Rho/(1+K*D/(E*e)));     % 波速
 dt=CFL*dx/c;                        % 单位计算时间
 lambda=dt/dx;
+u0=0.1;
 k=1;
 t=0;time(k)=t;
 
 % 第一行压力和第二行为流速
-U(k,2:NS)=0;
+U(k,2:NS)=Hr-(2:NS)*f*u0*u0*dx/(L*2*D);
+%U(k,2:NS)=0;
 U(k,1)=Hr;
-U(k+1,1:NS)=0;
+U(k+1,1:NS)=u0;
 
 while t<t_max
     for j=2:N
-        U(k+2:k+3,j)=U(k:k+1,j)+dt*S(U(k:k+1,j))...
+        U(k+2:k+3,j)=U(k:k+1,j)+lambda*S(U(k:k+1,j))...
             -lambda*(FluxGod(U(k:k+1,j+1),U(k:k+1,j))...
             -FluxGod(U(k:k+1,j),U(k:k+1,j-1)));
     end
@@ -44,10 +46,10 @@ while t<t_max
     % 边界值
 
     U(k+2,1)=Hr;                                % 压头第一列
-    U(k+3,1)=(U(k+2,1)-U(k+2,2))*g/c+U(k+3,2);   % 流量第一列数值，边界条件公式
+    U(k+3,1)=U(k+3,2)+g/c*(U(k+2,1)-U(k+2,2));   % 流量第一列数值，边界条件公式
 
     U(k+3,NS)=0;                                % 流量最后一列
-    U(k+2,NS)=U(k+2,NS-1)+c/g*U(k+3,NS-1);                               % 压头最后一列数值，边界条件公式
+    U(k+2,NS)=U(k+2,NS-1)+c/g*(U(k+3,NS-1)-U(k+3,NS));                               % 压头最后一列数值，边界条件公式
 
     lambda=dt/dx;
     t=t+dt;
